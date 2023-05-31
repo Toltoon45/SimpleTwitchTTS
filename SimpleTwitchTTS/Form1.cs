80196e91eed7cd6df00e33ce5765aec79b4a1012
EBLAN_IGRAN <EBLAN_IGRAN@DOLBAYEB>
2023-06-01 00:40:59 +0300

    Add project files.

---
+++ SimpleTwitchTTS/Form1.cs
@@ -0,0 +1,225 @@
+using System.Speech.Synthesis;
+using TwitchLib.Client;
+using TwitchLib.Communication.Events;
+
+namespace SimpleTwitchTTS
+{
+    public partial class MainWindow : Form
+    {
+        SpeechSynthesizer Synth = new SpeechSynthesizer();
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            textBoxTwitchApi.Text = Properties.Settings.Default.TextBoxTwitchApi;
+            textBoxTwitchNick.Text = Properties.Settings.Default.TextBoxTwitchNick;
+            checkBoxClearEmoji.Checked = Properties.Settings.Default.CheckBoxClearEmoji;
+            comboBoxInstalledVoices.Text = Properties.Settings.Default.ComboBoxInstalledVoiceSelect;
+            toolTipInfoForConnection.SetToolTip(comboBoxInstalledVoices, comboBoxInstalledVoices.Text);
+            trackBarTtsVolume.Value = Properties.Settings.Default.TtsVolume;
+            trackBarTtsSpeed.Value = Properties.Settings.Default.TtsSpeed;
+            textBoxViewerSkipAllQueueMessage.Text = Properties.Settings.Default.TtsSkipAll;
+            textBoxViewerSkipCurrentTtsMessage.Text = Properties.Settings.Default.TtsSkipCurrent;
+            textBoxDoNotTtsIfStartWith.Text = Properties.Settings.Default.TtsIgnore;
+
+            foreach (InstalledVoice voice in Synth.GetInstalledVoices())
+            {
+                comboBoxInstalledVoices.Items.Add(voice.VoiceInfo.Name);
+            }
+
+            if (!File.Exists("BlackList.txt"))
+                using (System.IO.FileStream fs = System.IO.File.Create("BlackList.txt")) ;
+            using (System.IO.StreamReader sr = new System.IO.StreamReader("BlackList.txt"))
+            {
+                while (!sr.EndOfStream)
+                {
+                    listBoxBlackList.Items.Add(sr.ReadLine());
+                }
+            }
+        }
+
+        private void linkLabelTwitchApi_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+            try
+            {
+                System.Diagnostics.Process.Start("explorer.exe", "https://twitchtokengenerator.com/");
+            }
+            catch
+            {
+                linkLabelTwitchApi.Text = "https://twitchtokengenerator.com/";
+                Clipboard.SetText(linkLabelTwitchApi.Text);
+                toolTipInfoForConnection.SetToolTip(linkLabelTwitchApi, "Copied, go to browser!");
+            }
+
+        }
+
+
+        private void textBoxTwitchApi_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void MainWindow_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            Properties.Settings.Default.TextBoxTwitchApi = textBoxTwitchApi.Text;
+            Properties.Settings.Default.TextBoxTwitchNick = textBoxTwitchNick.Text;
+            Properties.Settings.Default.CheckBoxClearEmoji = checkBoxClearEmoji.Checked;
+            Properties.Settings.Default.ComboBoxInstalledVoiceSelect = comboBoxInstalledVoices.Text;
+            Properties.Settings.Default.TtsVolume = trackBarTtsVolume.Value;
+            Properties.Settings.Default.TtsSpeed = trackBarTtsSpeed.Value;
+            Properties.Settings.Default.TtsSkipAll = textBoxViewerSkipAllQueueMessage.Text;
+            Properties.Settings.Default.TtsSkipCurrent = textBoxViewerSkipCurrentTtsMessage.Text;
+            Properties.Settings.Default.TtsIgnore = textBoxDoNotTtsIfStartWith.Text;
+            Properties.Settings.Default.Save();
+
+            using (System.IO.StreamWriter sw = new System.IO.StreamWriter("BlackList.txt"))
+            {
+                for (int i = 0; i < listBoxBlackList.Items.Count; i++)
+                    sw.WriteLine(listBoxBlackList.Items[i].ToString());
+            }
+        }
+
+        TwitchClient TClient = new TwitchClient();
+        TwitchBot TWbot = new TwitchBot();
+        bool Connect = false;
+
+
+
+        private void buttonTwitchConnect_Click(object sender, EventArgs e)
+        {
+            labelConnectionStatus.ForeColor = Color.Black;
+            labelConnectionStatus.Text = "Conntecing...";
+            if (Connect == false)
+            {
+                try
+                {
+                    TClient = TWbot.Connect(textBoxTwitchApi.Text, textBoxTwitchNick.Text, comboBoxInstalledVoices.Text, TClient);
+                    if (TClient != null)
+                    {
+                        TClient.OnDisconnected += TClientOnDisconnected;
+                        Connect = true;
+
+                        foreach (var item in listBoxBlackList.Items)
+                        {
+                            TWbot.AddToBlackList(Convert.ToString(item));
+                        }
+                    }
+                }
+                catch
+                {
+                    labelConnectionStatus.Text = "Did not connected";
+                    labelConnectionStatus.ForeColor = Color.Purple;
+                    Connect = false;
+                }
+
+                labelConnectionStatus.Text = (TClient != null) ? "Connected" : "Did not connect";
+                labelConnectionStatus.ForeColor = (TClient != null) ? Color.Green : Color.Purple;
+                Connect = false;
+            }
+
+        }
+        //change status of connection.
+        private void TClientOnDisconnected(object? sender, OnDisconnectedEventArgs e)
+        {
+
+            if (InvokeRequired)
+            {
+                this.Invoke(new Action(() => labelConnectionStatus.Text = "Disconnected"));
+                this.Invoke(new Action(() => labelConnectionStatus.ForeColor = Color.Red));
+            }
+            Connect = false;
+
+        }
+
+        internal void TWbotDisconnected()
+        {
+            labelConnectionStatus.Text = "Disconnected";
+            labelConnectionStatus.ForeColor = Color.Red;
+        }
+
+        private void checkBoxClearEmoji_CheckedChanged(object sender, EventArgs e)
+        {
+            TWbot.ClearEmoji(checkBoxClearEmoji.Checked);
+        }
+
+        private void comboBoxInstalledVoices_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            toolTipInfoForConnection.SetToolTip(comboBoxInstalledVoices, comboBoxInstalledVoices.Text);
+            TWbot.SetTtsName(comboBoxInstalledVoices.Text);
+        }
+
+        private void trackBarTtsVolume_Scroll(object sender, EventArgs e)
+        {
+
+        }
+
+        private void trackBarTtsVolume_ValueChanged(object sender, EventArgs e)
+        {
+            labelTtsVolume.Text = Convert.ToString(trackBarTtsVolume.Value);
+            TWbot.SetTtsVolume(trackBarTtsVolume.Value);
+        }
+
+        private void trackBarTtsSpeed_ValueChanged(object sender, EventArgs e)
+        {
+            labelTtsSpeed.Text = Convert.ToString(trackBarTtsSpeed.Value);
+            TWbot.SetTtsSpeed(trackBarTtsSpeed.Value);
+        }
+
+        private void buttonAddToBlackList_Click(object sender, EventArgs e)
+        {
+            if (!listBoxBlackList.Items.Contains(textBoxBlackList.Text))
+            {
+                listBoxBlackList.Items.Add(textBoxBlackList.Text);
+
+                TWbot.AddToBlackList(textBoxBlackList.Text.ToLower());
+            }
+        }
+
+        private void buttonRemoveFromBlackList_Click(object sender, EventArgs e)
+        {
+            int index = listBoxBlackList.SelectedIndex * -1;
+            TWbot.GetBlackListDeleteItem(listBoxBlackList.SelectedIndex);
+            listBoxBlackList.Items.RemoveAt(listBoxBlackList.SelectedIndex);
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void buttonClearEntireQueue_Click(object sender, EventArgs e)
+        {
+            TWbot.SpeakCancell();
+        }
+
+        private void buttonSkipCurrentMessage_Click(object sender, EventArgs e)
+        {
+            //TWbot.SpeakCancel();
+        }
+
+        private void textBoxViewerSkipCurrentTtsMessage_TextChanged(object sender, EventArgs e)
+        {
+            TWbot.ViewerSkipCurrentTtsMessage(textBoxViewerSkipCurrentTtsMessage.Text);
+        }
+
+
+        private void textBoxViewerSkipAllQueueMessage_TextChanged(object sender, EventArgs e)
+        {
+            TWbot.ViewerSkipAllTtsQueueMessage(textBoxViewerSkipAllQueueMessage.Text);
+        }
+
+        private void textBoxDoNotTtsIfStartWith_TextChanged(object sender, EventArgs e)
+        {
+            try
+            {
+                TWbot.DoNotTtsIfStartWith(Convert.ToChar(textBoxDoNotTtsIfStartWith.Text));
+            }
+            catch
+            {
+
+
+            }
+
+        }
+    }
+}
---
+++ SimpleTwitchTTS/TwitchBot.cs
@@ -0,0 +1,179 @@
+﻿using TwitchLib.Client;
+using TwitchLib.Client.Events;
+using TwitchLib.Client.Models;
+using System.Text.RegularExpressions;
+using System.Speech.Synthesis;
+
+namespace SimpleTwitchTTS
+{
+    internal class TwitchBot
+    {
+
+        string TwitchNick;
+        ConnectionCredentials Creds;
+        TwitchClient TClient;
+        bool TtsClearEmoji = false;
+
+        int TtsVolume;
+        int TtsSpeed;
+
+        List<string> BlackList = new List<string>();
+
+        //TClient needed to change connection status.
+        internal TwitchClient? Connect(string Api, string Nick, string VoiceName, TwitchClient client)
+        {
+            try
+            {
+                if (client != null)
+                {
+                    TClient = client;
+                    Creds = new ConnectionCredentials(Nick, Api);
+                    TwitchNick = Nick;
+                    SynthText.SelectVoice(VoiceName);
+
+                    TClient.OnConnected += TClientOnConnected;
+                    TClient.OnMessageReceived += TClientOnMessageReceived;
+
+                    TClient.Initialize(Creds, Nick);
+                    TClient.Connect();
+
+                    return TClient;
+                }
+                else { return null; }
+
+            }
+            catch
+            {
+                if (TClient != null)
+                {
+                    TClient.OnConnected -= TClientOnConnected;
+                    TClient.OnMessageReceived -= TClientOnMessageReceived;
+                }
+
+                return null;
+
+            }
+
+
+        }
+        //Part With TTS
+        string Message;
+        SpeechSynthesizer SynthText = new SpeechSynthesizer();
+        private void TClientOnMessageReceived(object? sender, OnMessageReceivedArgs e)
+        {
+            if (!BlackList.Contains(e.ChatMessage.Username) && e.ChatMessage.Message[0] != ttsIgnore)
+            {
+                if (ViewerSkipAllQueueMessage != "" && e.ChatMessage.Message == ViewerSkipAllQueueMessage)
+                {
+                    SynthText.SpeakAsyncCancelAll();
+                    return;
+                }
+
+                
+                if (ViewerSkipCurrentMessage != "" && e.ChatMessage.Message == ViewerSkipCurrentMessage)
+                {
+                    SpeakCancell();
+                    return;
+                }
+
+                //TTS will read long link as one word "link" instead of every letter
+                Message = e.ChatMessage.Message;
+                for (int i = 0; i < Message.Length - 1; i++)
+                {
+                    if (Message[i] == '.' && Message[i + 1] == '.')
+                    {
+                        Message = Message.Replace(Message[i], ' ');
+                    }
+
+                }
+                //Message = Regex.Replace(Message, "..", "");
+                Message = Regex.Replace(Message, @"(?:http(s)?:\/\/)?[\w.-]+\D(?:\.[\w\.-]+)+[\w\-\._~:/?%#[\]@!\$&'\(\)\*\+,;=.]+", "link");
+
+
+                //Russian TTS yuri crash the programm because of emoji.
+                //Replace emoji
+                if (TtsClearEmoji)
+                {
+                    Message = Regex.Replace(Message, @"\p{Cs}", "");
+                }
+                SynthText.SpeakAsync(Message);
+            }
+        }
+
+        private void TClientOnConnected(object? sender, OnConnectedArgs e)
+        {
+            try
+            {
+                TClient.SendMessage(TwitchNick, "gotovo");
+            }
+            catch
+            {
+
+            }
+
+        }
+
+
+
+        internal void ClearEmoji(bool @checked)
+        {
+            TtsClearEmoji = @checked;
+        }
+
+        internal void SetTtsVolume(int value)
+        {
+            SynthText.Volume = value;
+        }
+
+        internal void SetTtsSpeed(int value)
+        {
+            SynthText.Rate = value;
+        }
+
+        internal void SetTtsName(string text)
+        {
+            SynthText.SelectVoice(text);
+        }
+
+        internal void AddToBlackList(string v)
+        {
+            BlackList.Insert(BlackList.Count, v);
+        }
+
+        internal void GetBlackListDeleteItem(int v)
+        {
+            if (BlackList.Count <= 1)
+                BlackList.Clear();
+            else
+                BlackList.RemoveAt(v);
+        }
+
+        internal void SpeakCancell()
+        {
+            //skip only the current spoken message
+            var current = SynthText.GetCurrentlySpokenPrompt();
+
+            if (current != null)
+                SynthText.SpeakAsyncCancel(current);
+
+        }
+        string ViewerSkipCurrentMessage;
+        internal void ViewerSkipCurrentTtsMessage(string text)
+        {
+            ViewerSkipCurrentMessage = text;
+        }
+
+        string ViewerSkipAllQueueMessage;
+        internal void ViewerSkipAllTtsQueueMessage(string text)
+        {
+            ViewerSkipAllQueueMessage = text;
+        }
+        char ttsIgnore;
+        internal void DoNotTtsIfStartWith(char text)
+        {
+            try { ttsIgnore = text; }
+            catch { }
+
+        }
+    }
+}
